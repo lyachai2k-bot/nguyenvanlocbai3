73ffe18d80405aed415163f6d9b0824d33498293
Nguyen Van Loc <lyachai2k@gmail.com>
2026-03-05 11:20:36 +0700

    ok

--- nguyenvanlocbai3/Models/Product.cs
+++ nguyenvanlocbai3/Models/Product.cs
@@ -1,10 +1,18 @@
-﻿namespace nguyenvanlocbai3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nguyenvanlocbai3.Models
 {
     public class Product
     {
+
+        [Required]
+
         public int Id { get; set; }
+        [StringLength(100, MinimumLength = 3)]
         public string? Name { get; set; }
         public decimal Price { get; set; }
+        [Range(0.01, 1000, ErrorMessage = "Price must be between 0.01 and 1000.")]
         public string? Description { get; set; }
     }
+
 }
